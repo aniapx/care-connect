dfb5ca41594abd42bb171fcfc979447d02fdffd6
aniapx <anna.plust@gmail.com>
2023-04-16 10:05:27 +0200

    Added new component for payments

--- src/app/modules/payments/components/recent-payout/recent-payout.component.ts
+++ src/app/modules/payments/components/recent-payout/recent-payout.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./recent-payout.component.scss']
 })
 export class RecentPayoutComponent {
-
+  public payments = [
+    {
+      date: '2022-01-23',
+      method: 'Bank',
+      notes: '1123423454536',
+      amount: 123
+    },
+    {
+      date: '2022-09-23',
+      method: 'Card',
+      notes: '11AHJ93454536',
+      amount: 30
+    },
+    {
+      date: '2023-03-23',
+      method: 'Cash',
+      notes: '11AHJ93454536',
+      amount: 130
+    },
+    {
+      date: null,
+      method: '-',
+      notes: '-',
+      amount: 130
+    }
+  ];
 }
--- src/app/modules/payments/components/recent-treatment/recent-treatment.component.ts
+++ src/app/modules/payments/components/recent-treatment/recent-treatment.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ITreatmentOverview } from '@app/shared/models/treatment-overview';
 
 @Component({
   selector: 'app-recent-treatment',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./recent-treatment.component.scss']
 })
 export class RecentTreatmentComponent {
+
+  public treatments: ITreatmentOverview[] = [
+    {
+      name: 'Root Canal',
+      date: '2022-11-24',
+      price: 178,
+      patientName: 'Diane Cooper'
+    },
+    {
+      name: 'Bleaching',
+      date: '2022-11-23',
+      price: 50,
+      patientName: 'Ralph McCoy'
+    },
+    {
+      name: 'Scaling',
+      date: '2022-11-20',
+      price: 20,
+      patientName: 'John Smith'
+    }
+  ];
 
 }
