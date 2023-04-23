a3e12a3efecea7af297a4498e446f20ef5d94e8d
aniapx <anna.plust@gmail.com>
2023-04-23 17:20:41 +0200

    Finished payments

--- src/app/modules/payments/containers/payments/payments.component.ts
+++ src/app/modules/payments/containers/payments/payments.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as AppActions from '../../../../core/store';
 
 @Component({
   selector: 'app-payments',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./payments.component.scss']
 })
 export class PaymentsComponent {
+  private readonly page = {
+    title: 'Payment Information',
+    menuOptions: [],
+    id: 0,
+    icon: 'fa-solid fa-wallet',
+  };
 
+  constructor(private store: Store) {
+    this.store.dispatch(AppActions.setPage(this.page));
+  }
 }
